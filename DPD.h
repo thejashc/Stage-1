470cfef482bd4cc77a97a5502d81eb9e54d6ecdc
thejashc <thejashc@gmail.com>
2017-12-04 19:33:52 +0100

    NVE ensemble MD routine + VTK file writing module for particle position

--- DPD.h
+++ DPD.h
@@ -3,7 +3,9 @@
 
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 #include "Particle.h"
+#include <ctime>
 
 //declare DPD solver
 class DPD {
@@ -16,36 +18,40 @@ class DPD {
 		double rc2;				// square of the cutoff distance
 		double dt;				// time step
 
-		double step;				// counter for number of steps 
+		int step;				// counter for number of steps 
 		double stepMax;				// total number of steps
+		double pot_en;				// system potential energy
+		double kin_en;				// system kinetic energy
+		double tot_en;				// system total energy
 
-		unsigned int saveCount = 1;	     	//number of timestep between saves
+		unsigned int saveCount = 100;	     	//number of timestep between saves
 		std::vector<Particle> particles;     	//vector of particles
 
-		/*
-		// function to initialize position
-		void initialize_position(){
-
-		for (Particle& p : particles) {
-		// update position (integral time step) using the velocities (mid-step)
-		setZero(p.r);				
-		}
-		}*/
-
+		/******************************************************************************************************/
+		/**************************************** SOLVE ROUTINE ***********************************************/
+		/******************************************************************************************************/
 		//solve contains the time stepping algorithm and the output routines 
 		void solve () {
+			// cut-off correction to potential
+			double sig6 = pow(sigma,6);
+			double rc2i = 1/rc2;
+			double rc6i = pow(rc2i,3);
+			double ecut = 4*epsilon*sig6*rc6i*(sig6*rc6i - 1.0);
+
 			//initialise time, and counter/ofstream for data output
 			double t=0;
 			unsigned int counter = 0;
-			std::ofstream file("data");
+
+			std::ofstream file1("./data/en_data.dat");
+
 			while (step<stepMax) {
 
-				//loop over all contacts p=1..N, q=p+1..N to evaluate forces
-				for (auto p = particles.begin();  p!=particles.end(); ++p)
+				// force calculation
+				// forceCalc();
+				//loop over all contacts p=1..N-1, q=p+1..N to evaluate forces
+				for (auto p = particles.begin();  p!=particles.end()-1; ++p){
+					// for_loop_inner_counter = 0;
 					for (auto q = p+1;  q!=particles.end(); ++q) {
-						// double distance = sqrt((p->r-q->r).lengthSquared());
-						// double overlap = p->a+q->a-distance;
-						// if (overlap>0) {
 
 						Vec2D Rij = p->r - q->r;	
 						Vec2D temp;
@@ -55,57 +61,232 @@ class DPD {
 						double r2 = minRij.getLengthSquared();
 
 						if ( r2 < rc2 ) {
-							double sig6 = pow(sigma,6);
 							double r2i = 1/r2;
 							double r6i = pow(r2i,3);
-							double ff = 48*epsilon*sig6*r2i*r6i*(r6i - 0.5);
+							double ff = 48.0*epsilon*sig6*r2i*r6i*(r6i - 0.5);
 							p->f += ff*minRij;
 							q->f += -1.0*ff*minRij; 
+
+							// calculate the potential energy
+							double pair_pot_en = 4.0*epsilon*sig6*r6i*(sig6*r6i - 1.0);
+							pot_en += pair_pot_en - ecut;
 						}		
-						else{
-							p->f += 0;
-							q->f += 0;
-						}
-					}
 
-					// Integrate equations of motion using Verlet leap_frog method
-					for (Particle& p : particles) {
-						// store velocity (mid-step)
-						Vec2D w_old = p.w;
-
-						// update velocities (mid-step)
-						p.w += p.f*(dt/p.m);
-
-						// update position (integral time step) using the velocities (mid-step)
-						p.r += p.w*dt;				
-
-						// calculate velocity (integral time step)
-						p.v = 0.5*(w_old + p.w);
 
 					}
 
-					// Apply periodic boundary conditions
-					for (Particle& p : particles) {
-	
-						Vec2D temp;
-						temp.X = Vec2D::roundOff_x(p.r, box);
-						temp.Y = Vec2D::roundOff_y(p.r, box);
-						p.r  = p.r - temp*box;				
+				}
 
-					}
+				// Integrate equations of motion using Verlet leap_frog method
+				// integrateEOS();
+				for (Particle& p : particles) {
+					// store velocity (mid-step)
+					Vec2D w_old = p.w;
 
-					// increment time
-					step += 1;
+					// update velocities (mid-step)
+					p.w += p.f*(dt/p.m);
 
-					// print particle positions and velocities
-					for (Particle& p : particles) {
-						// update position (integral time step) using the velocities (mid-step)
-						file << p.r << "\n";				
-					}
+					// update position (integral time step) using the velocities (mid-step)
+					p.r += p.w*dt;				
 
-					} //end time loop
-				file.close();
-			} //end solve
-		};
+					// calculate velocity (integral time step)
+					p.v = 0.5*(w_old + p.w);
+
+					// calculate the kinetic energy
+					kin_en += 0.5*p.m*(p.v.getLengthSquared());
+
+				}
+
+
+				// total energy
+				tot_en = kin_en + pot_en;
+
+				// Apply periodic boundary conditions
+				// pbc();
+				for (Particle& p : particles) {
+
+					Vec2D temp;
+					temp.X = Vec2D::roundOff_x(p.r, box);
+					temp.Y = Vec2D::roundOff_y(p.r, box);
+					p.r  = p.r - temp*box;				
+
+				}					
+
+				//write output file in the .data format every 10th time step
+				if (++counter>=saveCount) {
+					//reset the counter, write time to terminal
+					counter = 0;
+
+					// particle positions in vtk file format
+					vtkFileWrite();
+
+					//writing the energy balance
+					file1<< pot_en << " " << kin_en << " " << tot_en << std::endl;
+				}
+
+				// reset energy and forces
+				// resetVar();
+				// energy reset to zero
+				pot_en = 0.0;
+				kin_en = 0.0;
+				tot_en = 0.0;
+
+				// set all forces to zero
+				for (Particle& p : particles) {
+					p.f.setZero();
+				}
+
+				// increment time
+				step += 1;
+
+			} //end time loop
+			file1.close();
+		} //end solve
+
+
+		/******************************************************************************************************/
+		/**************************************** FUNCTIONS ***************************************************/
+		/******************************************************************************************************/
+		/*
+		// Force Calculation
+		void forceCalc(){
+			//loop over all contacts p=1..N-1, q=p+1..N to evaluate forces
+			for (auto p = particles.begin();  p!=particles.end()-1; ++p){
+				// for_loop_inner_counter = 0;
+				for (auto q = p+1;  q!=particles.end(); ++q) {
+
+					Vec2D Rij = p->r - q->r;	
+					Vec2D temp;
+					temp.X = Vec2D::roundOff_x(Rij, box);
+					temp.Y = Vec2D::roundOff_y(Rij, box);
+					Vec2D minRij = Rij - temp*box;
+					double r2 = minRij.getLengthSquared();
+
+					if ( r2 < rc2 ) {
+						double r2i = 1/r2;
+						double r6i = pow(r2i,3);
+						double ff = 48.0*epsilon*sig6*r2i*r6i*(r6i - 0.5);
+						p->f += ff*minRij;
+						q->f += -1.0*ff*minRij; 
+
+						// calculate the potential energy
+						double pair_pot_en = 4.0*epsilon*sig6*r6i*(sig6*r6i - 1.0);
+						pot_en += pair_pot_en - ecut;
+					}		
+
+
+				}
+
+			}
+
+		}
+
+		// Integrating equations of motion -- Verlet Leap Frog scheme
+		void integrateEOS(){
+			for (Particle& p : particles) {
+				// store velocity (mid-step)
+				Vec2D w_old = p.w;
+
+				// update velocities (mid-step)
+				p.w += p.f*(dt/p.m);
+
+				// update position (integral time step) using the velocities (mid-step)
+				p.r += p.w*dt;				
+
+				// calculate velocity (integral time step)
+				p.v = 0.5*(w_old + p.w);
+
+				// calculate the kinetic energy
+				kin_en += 0.5*p.m*(p.v.getLengthSquared());
+
+			}
+
+		}
+
+		// Periodic boundary conditions
+		void pbc(){
+			for (Particle& p : particles) {
+
+				Vec2D temp;
+				temp.X = Vec2D::roundOff_x(p.r, box);
+				temp.Y = Vec2D::roundOff_y(p.r, box);
+				p.r  = p.r - temp*box;				
+
+			}					
+
+		}
+
+		// Reset variables
+		void resetVar(){
+			// energy reset to zero
+			pot_en = 0.0;
+			kin_en = 0.0;
+			tot_en = 0.0;
+
+			// set all forces to zero
+			for (Particle& p : particles) {
+				p.f.setZero();
+			}
+		}*/
+
+		// Filewriting for the VTK format
+		void vtkFileWrite(){
+
+			char filename[40];
+
+			sprintf( filename, "./data/data_%d.vtu", step);  
+			std::ofstream file(filename);
+			file << "<?xml version=\"1.0\"?>" << std::endl;
+			file << "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">" << std::endl;
+			file << "<UnstructuredGrid>";
+			file << "<Piece NumberOfPoints=\"100\" NumberOfCells=\"0\">" << std::endl;
+			file << "<Points>" << std::endl;
+			// position data
+			file << "<DataArray type=\"Float32\" Name=\"Position\" NumberOfComponents=\"3\" format=\"ascii\">"<< std::endl;
+			for (Particle& p: particles)
+			{
+				file << p.r <<" 0"<<std::endl;
+
+			}
+			file << "</DataArray>"<<std::endl;
+			file << "</Points>" << std::endl;	
+
+			// velocity data
+			file << "<PointData Vectors=\"vector\">"<< std::endl;	
+			file << "<DataArray type=\"Float32\" Name=\"Velocity\" NumberOfComponents=\"3\" format=\"ascii\">" << std::endl;
+			for (Particle& p: particles)
+			{
+				file <<  p.v <<" 0"<< std::endl;
+
+			}
+			file << "</DataArray>"<< std::endl;
+
+			// radius data
+			file << "<DataArray type=\"Float32\" Name=\"Radius\" format=\"ascii\">" << std::endl;
+			for (Particle& p: particles)
+			{
+				file <<  p.a << std::endl;
+
+			}
+			file << "</DataArray>"<< std::endl;
+			file << "</PointData>" << std::endl;	
+
+			// Cells data
+			file << "<Cells>"<< std::endl;	
+			file << "<DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\"></DataArray>" << std::endl;
+			file << "<DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\"></DataArray>" << std::endl;
+			file << "<DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\"></DataArray>" << std::endl;
+			file << "</Cells>" << std::endl;
+
+			// Piece
+			file << "</Piece>" << std::endl;
+			file << "</UnstructuredGrid>" << std::endl;
+			file << "</VTKFile>" << std::endl;
+
+			file.close();
+
+		}
+
+};
 
 #endif
