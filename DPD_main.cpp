084131d822af5e60ff0b3f8dc134ef9dfe9704d5
thejashc <thejashc@gmail.com>
2017-12-05 17:42:04 +0100

    3D Lennard-Jones MD main file

--- DPD_main.cpp
+++ DPD_main.cpp
@@ -7,47 +7,18 @@ int main () {
 	DPD coll2p;	
 
 	// set global parameters
-	coll2p.box = 6.0;
-	coll2p.epsilon = 1.0;				
-	coll2p.sigma = 1.0;			
-	coll2p.rcutoff = 3.0;				
-	coll2p.rc2 = pow(coll2p.rcutoff,2);		
+	coll2p.box = 6.0;			// dimension of box
+	coll2p.epsilon = 1.0;			// energy well depth
+	coll2p.sigma = 1.0;			// zero-potential distance
+	coll2p.rcutoff = 3.0;			// cut-off distance
+	coll2p.rc2 = pow(coll2p.rcutoff,2);	// square of cut-off distance
 
 	coll2p.dt = 1e-5;
 	coll2p.step = 1;
 	coll2p.stepMax = 5e5;
 
-	//initialize two particles with mass, position, velocity
-	double xind_min = -1.0*(coll2p.box/2.0) + 1.0;
-	double yind_min = -1.0*(coll2p.box/2.0) + 1.0;
-	double zind_min = -1.0*(coll2p.box/2.0) + 1.0;
-	double xind_max =  1.0*(coll2p.box/2.0);
-	double yind_max =  1.0*(coll2p.box/2.0);
-	double zind_max =  1.0*(coll2p.box/2.0);
-
-	double xind = xind_min;
-	// initialization
-	while ( xind < xind_max){
-		double yind = yind_min;
-		while( yind < yind_max){
-			double zind = zind_min;
-				while( zind < zind_max){
-					// generate random velocities
-					double rand_gen_velx = ((double) rand() / (RAND_MAX));
-					double rand_gen_vely = ((double) rand() / (RAND_MAX));
-					double rand_gen_velz = ((double) rand() / (RAND_MAX));
-
-					// initializing particle mass, radius, position and mid-step-velocity
-					coll2p.particles.push_back({0.2,1.0,{xind, yind, zind},{rand_gen_velx, rand_gen_vely, rand_gen_velz}});
-
-					// update zind
-					zind += 1.0;
-
-				}// end of zind
-				yind += 1.0;
-		}// end of yind			
-		xind += 1.0;
-	}// end of xind
+	// Initialization
+	coll2p.init();
 
 	// monitor total run time
 	tstart = time(0);
