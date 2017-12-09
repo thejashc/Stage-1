91364f83717a8d943b295b3f46531f4e7c9346ae
thejashc <thejashc@gmail.com>
2017-12-09 20:53:32 +0100

    3D MD LJ main file modular

--- DPD_main.cpp
+++ DPD_main.cpp
@@ -1,13 +1,13 @@
 #include "DPD.h"
 
 int main () {
-	
+
 	time_t tstart, tend;
 	//define an instance of the DPD solver
 	DPD coll2p;	
 
 	// set global parameters
-	coll2p.box = 6.0;			// dimension of box
+	coll2p.box = 10.0;			// dimension of box
 	coll2p.epsilon = 1.0;			// energy well depth
 	coll2p.sigma = 1.0;			// zero-potential distance
 	coll2p.rcutoff = 3.0;			// cut-off distance
@@ -15,10 +15,27 @@ int main () {
 
 	coll2p.dt = 1e-5;
 	coll2p.step = 1;
-	coll2p.stepMax = 5e5;
+	coll2p.stepMax = 2e5;
+
+	// cut-off correction to potential
+	coll2p.sig6 = pow(coll2p.sigma,6);
+	coll2p.rc2i = 1/coll2p.rc2;
+	coll2p.rc6i = pow(coll2p.rc2i,3);
+	coll2p.ecut = 4*coll2p.epsilon*coll2p.sig6*coll2p.rc6i*(coll2p.sig6*coll2p.rc6i - 1.0);
+	
+	// post-processing
+	coll2p.gR_radMin = 0.5;			// minimum radius for g(r) 
+	coll2p.gR_radDelta = 0.25;		// thickness of a shell
+	coll2p.gR_radMax = coll2p.box/2.0;		// maximum radius for g(r)
+	coll2p.gR_nElem  = round((coll2p.gR_radMax - coll2p.gR_radMin)/coll2p.gR_radDelta); // number of elements
+
+	coll2p.gR_tStart = 4e4;
+	coll2p.gR_tDelta = 100;
+	coll2p.gR_tEnd = coll2p.stepMax;
+	coll2p.gR_tSamples = round((coll2p.gR_tEnd - coll2p.gR_tStart)/(coll2p.gR_tDelta)) - 1;
 
 	// Initialization
-	coll2p.init();
+	coll2p.init();						// initialize particle positions and velocity
 
 	// monitor total run time
 	tstart = time(0);
