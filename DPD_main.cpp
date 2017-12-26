8a069ea18395799bed1eaa74b1484d49c0f7b0b4
thejashc <thejashc@gmail.com>
2017-12-26 12:55:12 +0100

    3D DPD with correct implementation of the Lowe thermostat

--- DPD_main.cpp
+++ DPD_main.cpp
@@ -15,11 +15,11 @@ int main () {
 	coll2p.rc2 = pow(coll2p.rcutoff,2);	// square of cut-off distance
 	coll2p.dim = 3;				// 3D system
 
-	coll2p.tau = 0.745;			// rate of thermalizing
+	coll2p.tau = 1.490;			// rate of thermalizing
 	coll2p.dt = 1e-2;
 	coll2p.step = 1;
-	coll2p.stepMax = 1e6;
-	coll2p.kBT = 1.0;			// Boltzmann constant
+	coll2p.stepMax = 5e5;
+	coll2p.kBT = 3.0;			// Boltzmann constant
 	coll2p.thermProb = coll2p.dt*coll2p.tau;// probability of thermalizing 	
 
 	// Cell list parameters
