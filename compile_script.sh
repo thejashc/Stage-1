426f48d3cd9d4063664cf61ace3a4290c38b70df
thejashc <thejashc@gmail.com>
2018-01-30 15:21:22 +0100

    updated compile script

--- compile_script.sh
+++ compile_script.sh
@@ -4,6 +4,7 @@
 rm -f *.txt
 rm -f *.o
 rm -f a.out
+rm -f *.exe
 rm -f *.dat
 rm -f ./data/*.vtu
 rm -f ./data/*.dat
@@ -14,6 +15,7 @@ g++ -o DPD.exe  -std=c++11 -ggdb3 Vec3D.cc DPD_main.cpp
 #g++ -o DPD.exe  -std=c++11 Vec3D.cc DPD_main.cpp 
 
 ./DPD.exe > simProg.txt &
+#./DPD.exe > randnum.dat &
 
 # valgrind for checking memory leaks and seg-faults
 #valgrind --leak-check=full --track-origins=yes -v ./DPD.exe
