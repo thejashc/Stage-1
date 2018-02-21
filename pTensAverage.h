fb170efea29482c0af1468ec978320f13ea63220
thejashc <thejashc@gmail.com>
2018-02-21 13:42:29 +0100

    splitting total pressure tensor calculation into ideal and non-Ideal components

--- pTensAverage.h
+++ pTensAverage.h
@@ -1,3 +1,4 @@
+/*
 pTensor[0][0] /= counter;
 pTensor[0][1] /= counter;
 pTensor[0][2] /= counter;
@@ -7,3 +8,26 @@ pTensor[1][2] /= counter;
 pTensor[2][0] /= counter;
 pTensor[2][1] /= counter;
 pTensor[2][2] /= counter;
+*/
+
+// ideal component
+pIdeal[0][0] /= counter;
+pIdeal[0][1] /= counter;
+pIdeal[0][2] /= counter;
+pIdeal[1][0] /= counter;
+pIdeal[1][1] /= counter;
+pIdeal[1][2] /= counter;
+pIdeal[2][0] /= counter;
+pIdeal[2][1] /= counter;
+pIdeal[2][2] /= counter;
+
+// non-ideal
+pNonIdeal[0][0] /= ( counter * volume );
+pNonIdeal[0][1] /= ( counter * volume );
+pNonIdeal[0][2] /= ( counter * volume );
+pNonIdeal[1][0] /= ( counter * volume );
+pNonIdeal[1][1] /= ( counter * volume );
+pNonIdeal[1][2] /= ( counter * volume );
+pNonIdeal[2][0] /= ( counter * volume );
+pNonIdeal[2][1] /= ( counter * volume );
+pNonIdeal[2][2] /= ( counter * volume );
