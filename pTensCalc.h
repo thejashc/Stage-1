924eca63b9faaf00fd32eb0c5f4accbfd754df03
thejashc <thejashc@gmail.com>
2018-02-21 13:43:15 +0100

    calculating the ideal and non-Ideal components of pressure separately

--- pTensCalc.h
+++ pTensCalc.h
@@ -1,7 +1,8 @@
-pIdeal[0][0] = rho * ( vx2 / npart ) ;		// equipartition theorem
-pIdeal[1][1] = rho * ( vy2 / npart ) ;
-pIdeal[2][2] = rho * ( vz2 / npart ) ;
+pIdeal[0][0] += rho * ( vx2 / npart ) ;		// equipartition theorem
+pIdeal[1][1] += rho * ( vy2 / npart ) ;
+pIdeal[2][2] += rho * ( vz2 / npart ) ;
 
+/*
 pTensor[0][0] += pIdeal[0][0] + ( 1. / volume ) * pNonIdeal[0][0];	// pxx
 pTensor[0][1] += pIdeal[0][1] + ( 1. / volume ) * pNonIdeal[0][1];	// pxy
 pTensor[0][2] += pIdeal[0][2] + ( 1. / volume ) * pNonIdeal[0][2];	// pxz
@@ -13,3 +14,4 @@ pTensor[1][2] += pIdeal[1][2] + ( 1. / volume ) * pNonIdeal[1][2];	// pyz
 pTensor[2][0] += pIdeal[2][0] + ( 1. / volume ) * pNonIdeal[2][0];	// pzx
 pTensor[2][1] += pIdeal[2][1] + ( 1. / volume ) * pNonIdeal[2][1];	// pzy
 pTensor[2][2] += pIdeal[2][2] + ( 1. / volume ) * pNonIdeal[2][2];	// pzz
+*/
