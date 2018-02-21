36bbfe9aab4a6d7e5e9da212484a315f409e476b
thejashc <thejashc@gmail.com>
2018-02-21 14:36:07 +0100

    resetting ideal and nonIdeal components of pressure tensor to 0

--- pTensReset.h
+++ pTensReset.h
@@ -1,3 +1,4 @@
+/*
 pTensor[0][0] = 0.0;
 pTensor[0][1] = 0.0;
 pTensor[0][2] = 0.0;
@@ -7,3 +8,26 @@ pTensor[1][2] = 0.0;
 pTensor[2][0] = 0.0;
 pTensor[2][1] = 0.0;
 pTensor[2][2] = 0.0;
+*/
+
+// ideal component
+pIdeal[0][0] = 0.0;
+pIdeal[0][1] = 0.0;
+pIdeal[0][2] = 0.0;
+pIdeal[1][0] = 0.0;
+pIdeal[1][1] = 0.0;
+pIdeal[1][2] = 0.0;
+pIdeal[2][0] = 0.0;
+pIdeal[2][1] = 0.0;
+pIdeal[2][2] = 0.0;
+
+// non-ideal
+pNonIdeal[0][0] = 0.0;
+pNonIdeal[0][1] = 0.0;
+pNonIdeal[0][2] = 0.0;
+pNonIdeal[1][0] = 0.0;
+pNonIdeal[1][1] = 0.0;
+pNonIdeal[1][2] = 0.0;
+pNonIdeal[2][0] = 0.0;
+pNonIdeal[2][1] = 0.0;
+pNonIdeal[2][2] = 0.0;
