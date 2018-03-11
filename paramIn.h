d5d4492842274f76e057315fd335e8193fb5e2d3
thejashc <thejashc@gmail.com>
2018-03-11 21:12:10 +0100

    module modified: reads parameters from the param.in file

--- paramIn.h
+++ paramIn.h
@@ -1,7 +1,108 @@
-// set global parameters
-boxEdge[x] = 20.0;
-boxEdge[y] = 20.0;
-boxEdge[z] = 20.0;
+std::cout << " ********************* READING PARAMETERS ************************* " << std::endl;	
+
+std::string const in_fld = "param.in";
+std::ifstream readParam (in_fld.c_str());
+if ( ! readParam  ) { std::cout << "*** input file is empty" << std::endl; abort(); }
+
+std::cout << "Reading Box Dimensions" << std::endl;
+readParam >> buffer >> boxEdge[x];	readParam.ignore(256,'\n');
+readParam >> buffer >> boxEdge[y];	readParam.ignore(256,'\n');
+readParam >> buffer >> boxEdge[z];	readParam.ignore(256,'\n');
+readParam >> buffer >> initRho;		readParam.ignore(256,'\n');
+
+// readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+
+std::cout << "Reading Liquid Liquid Interaction Parameters" << std::endl;
+readParam >> buffer >> Aij;		readParam.ignore(256,'\n');
+readParam >> buffer >> Bij;		readParam.ignore(256,'\n');
+readParam >> buffer >> rcutoff;		readParam.ignore(256,'\n');
+readParam >> buffer >> rd_cutoff;	readParam.ignore(256,'\n');
+
+// readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+#if WALL_ON
+
+std::cout << "Reading Solid Liquid Interaction Parameters" << std::endl;
+readParam >> buffer >> Asl;		readParam.ignore(256,'\n');
+readParam >> buffer >> Bsl;		readParam.ignore(256,'\n');
+readParam >> buffer >> rcWcutoff;	readParam.ignore(256,'\n');
+readParam >> buffer >> rdWcutoff;	readParam.ignore(256,'\n');
+
+std::cout << "Reading Solid geometrical Parameters" << std::endl;
+readParam >> buffer >> wallHeight;	readParam.ignore(256,'\n');
+readParam >> buffer >> initWallRho;	readParam.ignore(256,'\n');
+
+#else
+readParam.ignore(256, '\n');		// skip 8 lines 
+readParam.ignore(256, '\n');		
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');		
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');
+#endif
+//readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+
+#if RANDOM_DISSIPATIVE
+std::cout << "Reading Random and Dissipative Parameters" << std::endl;
+readParam >> buffer >> kBT;		readParam.ignore(256,'\n');
+readParam >> buffer >> sigma;		readParam.ignore(256,'\n');
+#else
+readParam.ignore(256, '\n');		// skip 3 lines - one + two
+readParam.ignore(256, '\n');		
+readParam.ignore(256, '\n');
+#endif
+readParam >> buffer >> dim;		readParam.ignore(256,'\n');
+
+//readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+
+std::cout << "Reading Time Parameters" << std::endl;
+readParam >> buffer >> dt;		readParam.ignore(256,'\n');
+readParam >> buffer >> stepMax;		readParam.ignore(256,'\n');
+
+//readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+
+std::cout << "Reading Structure Factor Parameters" << std::endl;
+readParam >> buffer >> gR_radMin;	readParam.ignore(256,'\n');
+readParam >> buffer >> gR_radDelta;	readParam.ignore(256,'\n');
+readParam >> buffer >> gR_tStart;	readParam.ignore(256,'\n');
+readParam >> buffer >> gR_tDelta;	readParam.ignore(256,'\n');
+
+//readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+
+std::cout << "Reading Velocity Distribution Parameters" << std::endl;
+readParam >> buffer >> velHist_velMin;		readParam.ignore(256,'\n');
+readParam >> buffer >> velHist_velDelta;	readParam.ignore(256,'\n');
+readParam >> buffer >> velHist_velMax;		readParam.ignore(256,'\n');
+readParam >> buffer >> velHist_tStart;		readParam.ignore(256,'\n');
+readParam >> buffer >> velHist_tDelta;		readParam.ignore(256,'\n');
+
+//readParam >> buffer >> emptyLine;	readParam.ignore(256,'\n');
+#if PLANAR_SLAB
+std::cout << "Reading Planar Slab Parameters" << std::endl;
+readParam >> buffer >> slabWidth;	readParam.ignore(256,'\n');
+readParam >> buffer >> rhoZ_Zmin;	readParam.ignore(256,'\n');
+readParam >> buffer >> rhoZ_Zdelta;	readParam.ignore(256,'\n');
+#else
+readParam.ignore(256, '\n');		// skip 4 lines -- one + three line
+readParam.ignore(256, '\n');		
+readParam.ignore(256, '\n');
+readParam.ignore(256, '\n');
+#endif
+
+#if CYLINDER_DROPLET
+std::cout << "Reading Cylindrical Slab Parameters" << std::endl;
+readParam >> buffer >> cylRad;		readParam.ignore(256,'\n');
+readParam >> buffer >> cylHeight;	readParam.ignore(256,'\n');
+readParam >> buffer >> rhor_rmin;	readParam.ignore(256,'\n');
+readParam >> buffer >> rhor_rdelta;	readParam.ignore(256,'\n');
+#else
+readParam.ignore(256, '\n');		// skip two lines
+readParam.ignore(256, '\n');
+#endif 
+
+readParam.close();
 
 // Half box size
 boxHalve[x] = boxEdge[x] / 2.0;
@@ -13,74 +114,31 @@ boxRecip[x] = 1.0 / boxEdge[x];
 boxRecip[y] = 1.0 / boxEdge[y];
 boxRecip[z] = 1.0 / boxEdge[z];
 
-kBT = 1.0;			// DPD fluid temperature 
-Aij = -40.0;			// DPD Warren conservative force -- attractive parameter
-Bij = +40.0;			// DPD Warren conservative force -- repulsive parameter
 #if RANDOM_DISSIPATIVE
-sigma = 3.0;			// DPD random force parameter
 gamma = pow( sigma,2.0 )/( 2.0 * kBT ); // DPD dissipative force parameter
 #endif
-rcutoff = 1.0;			// cut-off distance for pairwise attraction
-rd_cutoff = 0.75;			// cut-off distance for pairwise repulsion
+
 fifteen_by_twopi_by_rd = 15.0/( 2.0 * M_PI * pow( rd_cutoff,3.0) );	// 15/(2*PI*rd^3) used in Lucy weight function
 fifteen_by_twopi_by_rc = 15.0/( 2.0 * M_PI * pow( rcutoff,3.0 ) );	// 15/(2*PI*rc^3) used in Lucy weight function
 rc2 = pow( rcutoff , 2);	// square of cut-off distance
 rd2 = pow( rd_cutoff,2);	// square of cut-off distance
-dim = 3;				// 3D system
-
-dt = 0.001;
-step = 1;
-stepMax = 5e4;
-
-#if WALL_ON
-Asl = +0.0;
-Bsl = +20.0;
-fSL = 0.0;
-w1P = 0.0;
-w2P = 0.0;
-root2 = 0.3;
-fWallcutoff = -49.2188;
-
-rcWcutoff = 0.8;
-rdWcutoff = 0.7;
-rcWby2 = rcWcutoff / 2.;
-rdWby2 = rdWcutoff / 2.;
-rcW2 = pow(rcWcutoff, 2. );	// square of wall cutoff distance -- attractive
-rdW2 = pow(rdWcutoff, 2. );	// square of wall cutoff distance -- repulsive
-#endif
 
 // post-processing
 // g(r) calculation
-gR_radMin = 0.0;			// minimum radius for g(r) 
-gR_radDelta = 0.01;			// thickness of a shell
 gR_radMax = boxEdge[x] / 2.0;		// maximum radius for g(r)
 gR_nElem  = round( ( gR_radMax - gR_radMin )/ gR_radDelta); // number of elements
-
-gR_tStart = 5e4;
-gR_tDelta = 100;
 gR_tEnd = stepMax;
 gR_tSamples = round(( gR_tEnd - gR_tStart )/(gR_tDelta)) - 1;
 
 // velocity distribution
-velHist_velMin = -25.0;			// minimum velocity 
-velHist_velDelta = 0.005;		// resolution of DeltaVel
-velHist_velMax = 25.0;			// maximum velocity
 velHist_bins  = round( ( velHist_velMax - velHist_velMin)/velHist_velDelta); // number of elements
-
-velHist_tStart = 5e4;
-velHist_tDelta = 100;
 velHist_tEnd = stepMax;
 
 // density distribution along z
 #if PLANAR_SLAB
-rhoZ_Zmin 	= 0.00;		// minimum Z
-rhoZ_Zdelta 	= 0.05;		// resolution of Delta Z 
 rhoZ_Zmax 	= boxEdge[z];		// maximum Z
 rhoZ_bins  	= round( ( rhoZ_Zmax - rhoZ_Zmin ) / rhoZ_Zdelta ); // number of elements
 #elif CYLINDER_DROPLET
-rhor_rmin 	= 0.00;
-rhor_rdelta 	= 0.05;
 rhor_rmax 	= boxEdge[x];
 rhor_bins  	= round( ( rhor_rmax - rhor_rmin ) / rhor_rdelta ); // number of elements
-
 #endif
