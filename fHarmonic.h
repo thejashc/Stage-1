c797774c6be62cc8e10c3b4b03e20de563c543a8
thejashc <thejashc@gmail.com>
2018-04-05 08:42:41 +0200

    Harmonic force calculation for wall particles using nearest image convention

---
+++ fHarmonic.h
@@ -0,0 +1,17 @@
+Rij = particles[i].r - particles[i].r0;	
+
+Rij.X = Rij.X - boxEdge[x] * round( Rij.X / boxEdge[x] );
+Rij.Y = Rij.Y - boxEdge[y] * round( Rij.Y / boxEdge[y] );
+Rij.Z = Rij.Z - boxEdge[z] * round( Rij.Z / boxEdge[z] );
+
+r2 = Rij.getLengthSquared();
+dist = std::sqrt(r2);
+
+if ( dist != 0 )
+	capRij = Rij/dist;
+else
+	capRij.setZero();
+
+particles[i].fHarmonic.X = -1. * kWall * dist * capRij.X;	// spring force on the solid particle
+particles[i].fHarmonic.Y = -1. * kWall * dist * capRij.Y;	
+particles[i].fHarmonic.Z = -1. * kWall * dist * capRij.Z;	
