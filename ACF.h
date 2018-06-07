4c8fdcb914c1fab844febc728e8f88e665626f43
thejashc <thejashc@gmail.com>
2018-06-07 17:52:58 +0200

    Introduced the kinetic component of conservative stress of the form: u_{i, alpha} * u_{i, beta}

--- ACF.h
+++ ACF.h
@@ -8,15 +8,10 @@ for ( n_vars_counter=0 ; n_vars_counter<n_vars ; ++n_vars_counter ){
 }
 */
 
-// calculate components for stress-autocorrelation : 1 instead of pTensCounter
-// Sxx  = rho * ( vx2 / npart ) + ( pNonIdeal_temp[0][0] + pDissipative_temp[0][0] + pRandom_temp[0][0] ) / ( 1. * volume );
-// Syy  = rho * ( vy2 / npart ) + ( pNonIdeal_temp[1][1] + pDissipative_temp[1][1] + pRandom_temp[1][1] ) / ( 1. * volume );
-// Szz  = rho * ( vz2 / npart ) + ( pNonIdeal_temp[2][2] + pDissipative_temp[2][2] + pRandom_temp[2][2] ) / ( 1. * volume );
-
-SxyC   =  pNonIdeal_temp[0][1]     / ( volume );		// conservative force contribution to Sxy
-SxyR   =  pRandom_temp[0][1]       / ( volume );		// random force contribution to Sxy
-SxyD   =  pDissipative_temp[0][1]  / ( volume );		// dissipative force contribution to Sxy
-SxyVxy =  vxvy / volume;					// sum of v_{i,x} * v_{i,y} / volume
+SxyC   =  pNonIdeal_temp[0][1]     / volume;		// conservative force contribution to Sxy
+SxyR   =  pRandom_temp[0][1]       / volume;		// random force contribution to Sxy
+SxyD   =  pDissipative_temp[0][1]  / volume;		// dissipative force contribution to Sxy
+SxyVxy =  pNonIdealKin_temp[0][1]  / volume;					// sum of v_{i,x} * v_{i,y} / volume
 
 // Syz  = ( pNonIdeal_temp[1][2] + pDissipative_temp[1][2] + pRandom_temp[1][2] ) / ( 1. * volume );
 // Szx  = ( pNonIdeal_temp[2][0] + pDissipative_temp[2][0] + pRandom_temp[2][0] ) / ( 1. * volume );
@@ -27,11 +22,12 @@ SxyVxy =  vxvy / volume;					// sum of v_{i,x} * v_{i,y} / volume
 
 // std::cout << "Sxx= " << Sxx << ", Sxy= " << Sxy << ", Sxz= " << Szx << ", Syy=" << Syy << ", Syz= " << Syz << ", Szz= " << Szz << ", Nxy= " << Nxy << ", Nyz= " << Nyz << ", Nzx= " << Nzx << std::endl;
 // std::cout << SxyC << "\t" << SxyR << "\t" << SxyD << std::endl;
+// std::cout << SxyVxy << "\t\t\t" << SxyR << "\t\t\t" << SxyD << std::endl;
 
 
-//		Sxy	=	| Sxy_CC	Sxy_CR		Sxy_CD	|	Sxy matrix with all stress components
-//				| Sxy_RC	Sxy_RR		Sxy_RD	|
-//				| Sxy_DC	Sxy_DR		Sxy_DD	|			
+//		Sxy	=	| Sxy_CC	Sxy_CR		Sxy_CD |	Sxy matrix with all stress components
+//				| Sxy_RC	Sxy_RR		Sxy_RD |
+//				| Sxy_DC	Sxy_DR		Sxy_DD |			
 
 // call the auto-correlation function
 recursive_addCorr( SxyC,   0, 1 );
