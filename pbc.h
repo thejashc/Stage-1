eb49bd6c84442128a20ff2979f21bed7a43028a5
thejashc <thejashc@gmail.com>
2018-07-03 13:44:09 +0200

    the left and right end of the capillary tube is sealed-off to ensure that no particles escape

--- pbc.h
+++ pbc.h
@@ -1,4 +1,5 @@
 #if WALL_ON	// pbc only in x and y and z
+
 	mx = (int) round( ( particles[i].r.X - boxHalve[x] ) * boxRecip[x] );
 	my = (int) round( ( particles[i].r.Y - boxHalve[y] ) * boxRecip[y] );
 	mz = (int) round( ( particles[i].r.Z - boxHalve[z] ) * boxRecip[z] );
@@ -19,7 +20,8 @@
 	//		(a) Only lower wall present
 	//		(b) is that the z-velocity of the particle is +ve
 	#if LOWER_WALL_ON && !(UPPER_WALL_ON)
-		if ( abs(mz) > 1.0 ) {
+
+		if ( abs(mz) == 1.0 ) {
 
 			zOld 			= particles[i].r.Z - particles[i].w.Z * dt;	// calculate the previous z-position
 			tApp 			= ( boxEdge[z] - zOld ) / particles[i].w.Z; 	// time in which particle approaches from top face of the box
@@ -31,6 +33,36 @@
 
 			particles[i].r.Z 	= boxEdge[z] +  particles[i].w.Z * tSep ;	// finally, integrate EOM from the top face of the box with reverse velocity
 		}
+
+	#endif
+	#if CAPILLARY_TUBE
+		
+		if ( abs(mz) == 1.0 ) {
+
+			// simProg << " Particle " << i << " has temporarily gone out of the box " << std::endl;
+			
+			// simProg << " initial particle position = " << particles[i].r.Z << ", initial particle velocity = " << particles[i].w.Z << std::endl;
+
+			zOld 			= particles[i].r.Z - particles[i].w.Z * dt;	// calculate the previous z-position
+			tApp 			= ( particles[i].w.Z > 0. ) ? ( ( boxEdge[z] - zOld ) / particles[i].w.Z ) : ( zOld / fabs( particles[i].w.Z ) ); 
+			tSep 			= dt - tApp;					// time in which particle separates from top face of the box
+			particles[i].w.Z 	*= -1.0;					// reverse the velocity of the particle: away from the top of the box
+
+			// simProg << " zOld = " << zOld << ", tApp = " << tApp << ", tSep = " << tSep << std::endl;
+
+			if ( tSep < 0.0 || tSep > dt ){
+				simProg << "** tSep is     = " 	<< tSep << " **" << std::endl;
+				simProg << "** tApp is     = " 	<< tApp << " **" << std::endl;
+				simProg << "** velocity is = " 	<< particles[i].w.Z << " **" << std::endl;
+				simProg << "** zOld is     = " 	<< zOld << " **" << std::endl;
+				abort();
+			}
+
+			particles[i].r.Z 	= ( particles[i].w.Z < 0. ) ? ( boxEdge[z] +  particles[i].w.Z * tSep ) : ( particles[i].w.Z * tSep ) ;	// finally, integrate EOM from the top face of the box with reverse velocity
+			
+			// simProg << " final particle position = " << particles[i].r.Z << ", initial particle velocity = " << particles[i].w.Z << std::endl;
+		}
+
 	#endif
 
 	//if ( ( particles[i].r.Z < wallLowPos - 2*wallPenetration ) || ( particles[i].r.Z > wallTopPos + 2*wallPenetration ) )
