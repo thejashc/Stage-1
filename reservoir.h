5350bacc16c22160fed74371df85d1dcf134665f
thejashc <thejashc@gmail.com>
2018-07-20 11:22:39 +0200

    added a module for Piston initialization

--- reservoir.h
+++ reservoir.h
@@ -1,3 +1,54 @@
+#if PISTON
+	xind_min = 0.;
+	yind_min = 0.;
+	zind_min = pistonStart; 
+	xind_max = boxEdge[x];
+	yind_max = boxEdge[y];
+	zind_max = pistonEnd;
+
+	pCount = 0;
+
+	aCube = pow( 1. / initWallRho, 1./3. );
+
+	xind = xind_min;
+
+	simProg << "***************************************************" << std::endl;
+	simProg << "Started initialization of piston" << std::endl;
+	
+	simProg << "The minimum x-coord of piston is: "<< xind_min << std::endl;
+	simProg << "The minimum y-coord of piston is: "<< yind_min << std::endl;
+	simProg << "The minimum z-coord of piston is: "<< zind_min << std::endl;
+	
+	simProg << "The maximum x-coord of piston is: "<< xind_max << std::endl;
+	simProg << "The maximum y-coord of piston is: "<< yind_max << std::endl;
+	simProg << "The maximum z-coord of piston is: "<< zind_max << std::endl;
+	
+	xind = xind_min;
+	// Particle position intialization in a crystal structure 
+	while ( xind < xind_max){
+		yind = yind_min;
+		while( yind < yind_max){
+			zind = zind_min;
+			while( zind < zind_max){
+	
+				// initializing particle radius, mass, position and velocity
+				particles.push_back( { 1.0, 1.0, {xind, yind, zind}, {0., 0., 0.}, 0} );
+				Asl.push_back( { asl } );	// lyophilic wall 
+				pCount++;
+
+				// update zind
+				zind += aCube * rcutoff;
+
+			} // end of zind
+			yind += aCube * rcutoff;
+		} // end of yind			
+		xind += aCube * rcutoff;
+	} // end of xind
+
+	simProg << "finished initialization of  " << pCount << " inside piston" << std::endl;
+	simProg << "***************************************************" << std::endl;
+#endif
+
 // Set max and min dimensions of boxy
 xind_min = 0.;
 yind_min = 0.;
@@ -49,3 +100,4 @@ while ( xind < xind_max){
 
 simProg << "finished initialization of  " << pCount << std::endl;
 simProg << "***************************************************" << std::endl;
+
