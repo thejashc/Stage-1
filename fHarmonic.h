bce237948238818876ab7551bd595ebfa982f440
thejashc <thejashc@gmail.com>
2018-07-20 11:17:58 +0200

    made to changes to the way the solid particles are indexed

--- fHarmonic.h
+++ fHarmonic.h
@@ -1,4 +1,4 @@
-Rij = particles[i].r - particles[i].r0;	
+Rij = particles[solid_index[i]].r - particles[solid_index[i]].r0;	
 
 Rij.X = Rij.X - boxEdge[x] * round( Rij.X / boxEdge[x] );
 Rij.Y = Rij.Y - boxEdge[y] * round( Rij.Y / boxEdge[y] );
@@ -12,6 +12,6 @@ if ( dist != 0 )
 else
 	capRij.setZero();
 
-particles[i].fHarmonic.X = -1. * kWall * dist * capRij.X;	// spring force on the solid particle
-particles[i].fHarmonic.Y = -1. * kWall * dist * capRij.Y;	
-particles[i].fHarmonic.Z = -1. * kWall * dist * capRij.Z;	
+particles[solid_index[i]].fHarmonic.X = -1. * kWall * dist * capRij.X;	// spring force on the solid particle
+particles[solid_index[i]].fHarmonic.Y = -1. * kWall * dist * capRij.Y;	
+particles[solid_index[i]].fHarmonic.Z = -1. * kWall * dist * capRij.Z;	
