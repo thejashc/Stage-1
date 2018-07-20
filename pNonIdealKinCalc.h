e40d38790388810953e46453dd5e1cd4720db255
thejashc <thejashc@gmail.com>
2018-07-20 11:19:17 +0200

    accesing the fluid particles by their index using fluid_index[i]

--- pNonIdealKinCalc.h
+++ pNonIdealKinCalc.h
@@ -12,17 +12,17 @@ back to the actual definitions.
 */
 
 // Non-Ideal contribution to pressure -- Conservative forces	
-pNonIdealKin_temp[0][0] 	+= particles[i].v.X * particles[i].v.X;
-pNonIdealKin_temp[0][1] 	+= particles[i].v.X * particles[i].v.Y; 
-pNonIdealKin_temp[0][2] 	+= particles[i].v.X * particles[i].v.Z;
+pNonIdealKin_temp[0][0] 	+= particles[fluid_index[i]].v.X * particles[fluid_index[i]].v.X;
+pNonIdealKin_temp[0][1] 	+= particles[fluid_index[i]].v.X * particles[fluid_index[i]].v.Y; 
+pNonIdealKin_temp[0][2] 	+= particles[fluid_index[i]].v.X * particles[fluid_index[i]].v.Z;
 
-pNonIdealKin_temp[1][0] 	+= particles[i].v.Y * particles[i].v.X;
-pNonIdealKin_temp[1][1] 	+= particles[i].v.Y * particles[i].v.Y;
-pNonIdealKin_temp[1][2] 	+= particles[i].v.Y * particles[i].v.Z;
+pNonIdealKin_temp[1][0] 	+= particles[fluid_index[i]].v.Y * particles[fluid_index[i]].v.X;
+pNonIdealKin_temp[1][1] 	+= particles[fluid_index[i]].v.Y * particles[fluid_index[i]].v.Y;
+pNonIdealKin_temp[1][2] 	+= particles[fluid_index[i]].v.Y * particles[fluid_index[i]].v.Z;
 
-pNonIdealKin_temp[2][0] 	+= particles[i].v.Z * particles[i].v.X;
-pNonIdealKin_temp[2][1] 	+= particles[i].v.Z * particles[i].v.Y;
-pNonIdealKin_temp[2][2] 	+= particles[i].v.Z * particles[i].v.Z;
+pNonIdealKin_temp[2][0] 	+= particles[fluid_index[i]].v.Z * particles[fluid_index[i]].v.X;
+pNonIdealKin_temp[2][1] 	+= particles[fluid_index[i]].v.Z * particles[fluid_index[i]].v.Y;
+pNonIdealKin_temp[2][2] 	+= particles[fluid_index[i]].v.Z * particles[fluid_index[i]].v.Z;
 
-// pNonIdealKin_temp[0][1] 	+= ( particles[i].v.X * particles[i].v.Y ) * ( ( particles[i].r.Y >= 0.25 * boxEdge[y] ) && ( particles[i].r.Y <= 0.75 * boxEdge[y] )  );
-// pNonIdealKin_temp[1][0] 	+= ( particles[i].v.X * particles[i].v.Y ) * ( ( particles[i].r.Y < 0.25 * boxEdge[y] ) || ( particles[i].r.Y > 0.75 * boxEdge[y] )  );
+// pNonIdealKin_temp[0][1] 	+= ( particles[fluid_index[i]].v.X * particles[fluid_index[i]].v.Y ) * ( ( particles[fluid_index[i]].r.Y >= 0.25 * boxEdge[y] ) && ( particles[fluid_index[i]].r.Y <= 0.75 * boxEdge[y] )  );
+// pNonIdealKin_temp[1][0] 	+= ( particles[fluid_index[i]].v.X * particles[fluid_index[i]].v.Y ) * ( ( particles[fluid_index[i]].r.Y < 0.25 * boxEdge[y] ) || ( particles[fluid_index[i]].r.Y > 0.75 * boxEdge[y] )  );
