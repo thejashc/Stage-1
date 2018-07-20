3bfad28c82a04e1343fcd1e5fbd3ffab4b870863
thejashc <thejashc@gmail.com>
2018-07-20 11:25:46 +0200

    accesing the solid particles by their index using solid_index[i]

--- pbcWall.h
+++ pbcWall.h
@@ -1,6 +1,6 @@
-mx = (int) round( ( particles[i].r.X - boxHalve[x] ) * boxRecip[x] );	
-my = (int) round( ( particles[i].r.Y - boxHalve[y] ) * boxRecip[y] );
-// mz = (int) round( ( particles[i].r.Z - boxHalve[z] ) * boxRecip[z] );
+mx = (int) round( ( particles[solid_index[i]].r.X - boxHalve[x] ) * boxRecip[x] );	
+my = (int) round( ( particles[solid_index[i]].r.Y - boxHalve[y] ) * boxRecip[y] );
+// mz = (int) round( ( particles[solid_index[i]].r.Z - boxHalve[z] ) * boxRecip[z] );
 
 if ( abs(mx) > 1 || abs(my) > 1 )     // unacceptably large displacement
 {
@@ -9,6 +9,6 @@ if ( abs(mx) > 1 || abs(my) > 1 )     // unacceptably large displacement
 	abort();
 } // abs(mx)
 
-particles[i].r.X -= mx * boxEdge[x];    // apply periodic boundary conditions
-particles[i].r.Y -= my * boxEdge[y];
-// particles[i].r.Z -= mz * boxEdge[z];
+particles[solid_index[i]].r.X -= mx * boxEdge[x];    // apply periodic boundary conditions
+particles[solid_index[i]].r.Y -= my * boxEdge[y];
+// particles[solid_index[i]].r.Z -= mz * boxEdge[z];
