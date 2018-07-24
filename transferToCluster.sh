4f30006d2e49621ac2abadbdcad890daacb21190
thejashc <thejashc@gmail.com>
2018-07-24 10:44:23 +0200

    script to transfer executables to the cluster

---
+++ transferToCluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#scp -r {DPD.exe,plot_script.py,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/Green_Kubo #filenames should not contain commas in between
+scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
