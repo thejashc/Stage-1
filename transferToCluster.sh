14d5894e49913ea4cdca87ad03b1df87ed307498
thejashc <thejashc@gmail.com>
2018-07-26 11:48:37 +0200

    added lines for transferring files to cluster

--- transferToCluster.sh
+++ transferToCluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #scp -r {DPD.exe,plot_script.py,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/Green_Kubo #filenames should not contain commas in between
-scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
+#scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
+scp -r {DPD.exe,plot_script.py} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
