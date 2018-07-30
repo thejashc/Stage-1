bfadd3188bd28502fa0bdcbd1b8c49fd92777691
thejashc <thejashc@gmail.com>
2018-07-30 14:51:46 +0200

    transerring to clusteR

--- transferToCluster.sh
+++ transferToCluster.sh
@@ -2,4 +2,4 @@
 
 #scp -r {DPD.exe,plot_script.py,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/Green_Kubo #filenames should not contain commas in between
 #scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
-scp -r {DPD.exe,plot_script.py} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
+scp -r DPD.exe thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/squareCapillary #filenames should not contain commas in between
