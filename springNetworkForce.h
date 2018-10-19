b4942ceed36da6ff2a3eac956fb58362bb1fede5
thejashc <thejashc@gmail.com>
2018-10-19 12:02:38 +0200

    corrected the BUG : now calculates the spring force as k ( r - r0 ) where both r and r0 are scalars instead of vectors

--- springNetworkForce.h
+++ springNetworkForce.h
@@ -1,25 +1,47 @@
 i=0;
 while ( i < solidCount )
 {
-    for ( k=1; k<= particles[solid_index[i]].bondIndex[0]; ++k ) {
+    // std::cout << "**************" <<" particle i = " << i << "**************" << std::endl;
+
+    // for ( k=1; k<= particles[solid_index[i]].bondIndex[0]; ++k ) {
+    for ( k=1; k<= particles[i].bondIndex[0]; k++ ) {
 
         j = particles[solid_index[i]].bondIndex[k];     // jth particle
+        // j = particles[i].bondIndex[k];     // jth particle
 
-        Rij.X = particles[solid_index[i]].r.X - particles[solid_index[j]].r.X - particles[solid_index[i]].equilibriumBondLength[k][0];     // basically, finding r - r0 vector
-        Rij.Y = particles[solid_index[i]].r.Y - particles[solid_index[j]].r.Y - particles[solid_index[i]].equilibriumBondLength[k][1];
-        Rij.Z = particles[solid_index[i]].r.Z - particles[solid_index[j]].r.Z - particles[solid_index[i]].equilibriumBondLength[k][2];
+        // find the vector separating the particles
+        Rij = particles[solid_index[i]].r - particles[solid_index[j]].r;
+        // Rij = particles[i].r - particles[j].r;
 
         // nearest image distance
-        Rij.X = Rij.X - boxEdge[x] * round( Rij.X / boxEdge[x] );		// rij shear-flow correction : dR
+        Rij.X = Rij.X - boxEdge[x] * round( Rij.X / boxEdge[x] );
         Rij.Y = Rij.Y - boxEdge[y] * round( Rij.Y / boxEdge[y] );
         Rij.Z = Rij.Z - boxEdge[z] * round( Rij.Z / boxEdge[z] );
 
-        r2 = Rij.getLengthSquared();
+        // find the extension in length
+        r2     = Rij.getLengthSquared();
+        ext    = std::sqrt( r2 );
+        refExt = particles[i].eqBondLength[k];
 
-        particles[solid_index[i]].fHarmonic += -kWall * Rij;
-        particles[solid_index[j]].fHarmonic +=  kWall * Rij;
+        /*
+        std::cout << " particle j = " << j << "\t"
+                  << " ext = " << ext << "\t" 
+                  << " refExt = " << refExt << std::endl;
+                  */
 
-        springPotEn += 0.5 * kWall * r2; 
+        // find the unit vector
+        capRij = Rij/ ext;
+
+        // net-force
+        springForce = -kWall * ( ext - refExt ) * capRij;
+
+        // calculate the force as extension of length
+        particles[solid_index[i]].fHarmonic += springForce;
+        particles[solid_index[j]].fHarmonic -= springForce; 
+        // particles[i].fHarmonic += springForce;
+        // particles[j].fHarmonic -= springForce; 
+
+        springPotEn += 0.5 * kWall * pow( ext - refExt, 2. ); 
     }
 
     i++;
