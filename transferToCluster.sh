f89760f1e01d03096f07d8515783c5cce5d2ecbc
thejashc <thejashc@gmail.com>
2019-03-19 14:46:37 +0100

    removed unnecessary lines

--- transferToCluster.sh
+++ transferToCluster.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-destination='/storage3/usr/people/thejas/mDPD_parameterization/contactAngle/singleComponentDroplets/'
-destination='/storage3/usr/people/thejas/mDPD_parameterization/contactAngle/binaryMixtureDroplets'
-destination='/storage3/usr/people/thejas/imbibition/multiwetting/frozenParticleSimulations'
-#scp -r {DPD.exe,plot_script.py,param.in} thejas@msm2.ctw.utwente.nl:/storage3/usr/people/thejas/Green_Kubo #filenames should not contain commas in between
-#scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:$destination #filenames should not contain commas in between
-scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:$destination #filenames should not contain commas in between
-#scp -r {DPD.exe,param.in} thejas@msm2.ctw.utwente.nl:$destination/lowVisc_sigma_6_12/ #filenames should not contain commas in between
+destination='/storage3/usr/people/thejas/mDPD_parameterization/contactAngle/colloidalDroplets/'
 
-#scp -r {DPD.exe,param.in,readConfig/multipleColloids/Lx_20_Ly_20_Lz_50/*} thejas@msm2.ctw.utwente.nl:$destination/ #filenames should not contain commas in between
-#scp -r visualizeCapillary.tcl thejas@msm2.ctw.utwente.nl:$destination/ #filenames should not contain commas in between
+#scp -r {DPD.exe,visualizeCapillary.tcl,param.in} thejas@msm2.ctw.utwente.nl:$destination #filenames should not contain commas in between
+scp -r DPD.exe thejas@msm2.ctw.utwente.nl:$destination #filenames should not contain commas in between
+#scp -r readConfig/ thejas@msm2.ctw.utwente.nl:$destination #filenames should not contain commas in between
