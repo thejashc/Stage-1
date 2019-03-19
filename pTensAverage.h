49a407f37ac8c8118a94c968e384f82e318e9779
thejashc <thejashc@gmail.com>
2019-03-19 14:40:25 +0100

    added contribution to pressure from bond Interactions of the colloids

--- pTensAverage.h
+++ pTensAverage.h
@@ -2,9 +2,11 @@
 pIdeal[0][0] /= pcounter;
 pIdeal[0][1] /= pcounter;
 pIdeal[0][2] /= pcounter;
+
 pIdeal[1][0] /= pcounter;
 pIdeal[1][1] /= pcounter;
 pIdeal[1][2] /= pcounter;
+
 pIdeal[2][0] /= pcounter;
 pIdeal[2][1] /= pcounter;
 pIdeal[2][2] /= pcounter;
@@ -13,9 +15,11 @@ pIdeal[2][2] /= pcounter;
 pNonIdeal[0][0] /= ( pcounter * volume );
 pNonIdeal[0][1] /= ( pcounter * volume );
 pNonIdeal[0][2] /= ( pcounter * volume );
+
 pNonIdeal[1][0] /= ( pcounter * volume );
 pNonIdeal[1][1] /= ( pcounter * volume );
 pNonIdeal[1][2] /= ( pcounter * volume );
+
 pNonIdeal[2][0] /= ( pcounter * volume );
 pNonIdeal[2][1] /= ( pcounter * volume );
 pNonIdeal[2][2] /= ( pcounter * volume );
@@ -24,9 +28,11 @@ pNonIdeal[2][2] /= ( pcounter * volume );
 pNonIdealKin[0][0] /= ( pcounter * volume );
 pNonIdealKin[0][1] /= ( pcounter * volume );
 pNonIdealKin[0][2] /= ( pcounter * volume );
+
 pNonIdealKin[1][0] /= ( pcounter * volume );
 pNonIdealKin[1][1] /= ( pcounter * volume );
 pNonIdealKin[1][2] /= ( pcounter * volume );
+
 pNonIdealKin[2][0] /= ( pcounter * volume );
 pNonIdealKin[2][1] /= ( pcounter * volume );
 pNonIdealKin[2][2] /= ( pcounter * volume );
@@ -35,9 +41,11 @@ pNonIdealKin[2][2] /= ( pcounter * volume );
 pDissipative[0][0] /= ( pcounter * volume );
 pDissipative[0][1] /= ( pcounter * volume );
 pDissipative[0][2] /= ( pcounter * volume );
+
 pDissipative[1][0] /= ( pcounter * volume );
 pDissipative[1][1] /= ( pcounter * volume );
 pDissipative[1][2] /= ( pcounter * volume );
+
 pDissipative[2][0] /= ( pcounter * volume );
 pDissipative[2][1] /= ( pcounter * volume );
 pDissipative[2][2] /= ( pcounter * volume );
@@ -46,9 +54,24 @@ pDissipative[2][2] /= ( pcounter * volume );
 pRandom[0][0] /= ( pcounter * volume );
 pRandom[0][1] /= ( pcounter * volume );
 pRandom[0][2] /= ( pcounter * volume );
+
 pRandom[1][0] /= ( pcounter * volume );
 pRandom[1][1] /= ( pcounter * volume );
 pRandom[1][2] /= ( pcounter * volume );
+
 pRandom[2][0] /= ( pcounter * volume );
 pRandom[2][1] /= ( pcounter * volume );
 pRandom[2][2] /= ( pcounter * volume );
+
+// pBondInteractions 
+pBondInteractions[0][0] /= ( pcounter * volume );
+pBondInteractions[0][1] /= ( pcounter * volume );
+pBondInteractions[0][2] /= ( pcounter * volume );
+
+pBondInteractions[1][0] /= ( pcounter * volume );
+pBondInteractions[1][1] /= ( pcounter * volume );
+pBondInteractions[1][2] /= ( pcounter * volume );
+
+pBondInteractions[2][0] /= ( pcounter * volume );
+pBondInteractions[2][1] /= ( pcounter * volume );
+pBondInteractions[2][2] /= ( pcounter * volume );
