6620d7af05bc03427d1b2e3402b70caceb477439
thejashc <thejashc@gmail.com>
2019-07-22 14:30:37 +0200

    reading an fcc structure for the initial crystal -- ensuring a minimum energy

--- crystalInit.h
+++ crystalInit.h
@@ -1,133 +1,50 @@
-// Set max and min dimensions of boxy
-xind_min = 0.25;
-yind_min = 0.25;
-zind_min = 0.25;
-xind_max = boxEdge[x];
-yind_max = boxEdge[y];
-zind_max = boxEdge[z];
-
-npart = round( initRho * boxEdge[x] * boxEdge[y] * boxEdge[z] );
-
-#if MULTI_VISCOSITY_LIQUIDS
-    fluidType1_idxStart = 0;
-    fluidType1_idxEnd   = ( npart / 2 ) - 1;
-    fluidType2_idxStart = npart / 2;
-    fluidType2_idxEnd   = npart - 1;
-#endif 
+// read position of a fcc cylinder
+char posfname[100];
 
 /*
-aCube = pow( 1. / initRho, 1./3. );
-
-xind = xind_min;
-// Particle position intialization in a crystal structure 
-while ( xind < xind_max){
-	yind = yind_min;
-	while( yind < yind_max){
-		zind = zind_min;
-		while( zind < zind_max){
-			// generate random velocities
-			rand_gen_velx = ((double) rand() / (RAND_MAX));
-			rand_gen_vely = ((double) rand() / (RAND_MAX));
-			rand_gen_velz = ((double) rand() / (RAND_MAX));
-
-			// initializing particle radius, mass, position and velocity
-			particles.push_back({1.0,1.0,{xind, yind, zind},{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 1});
-
-			// update zind
-			zind += aCube*rcutoff;
-
-		}// end of zind
-		yind += aCube*rcutoff;
-	}// end of yind			
-	xind += aCube*rcutoff;
-}// end of xind
+readConfig >> dummyString;
+readConfig >> dummyString;
+readConfig >> dummyString;
+readConfig >> dummyString;
 */
 
+/***************************** Single Component / Multi component fluid fcc crystal *************************************/
+sprintf(posfname, "/storage/thejas/src/readConfig/fluids/CRYSTAL/fccCrystalSC_boxSize_%d.dat", int(boxEdge[x]) );
 
-#if MULTI_VISCOSITY_LIQUIDS
-    simProg << "***************************************************" << std::endl;
-    simProg << "Started initialization of the multi-component FCC crystal (positions and attractive powers)" << std::endl;
+std::ifstream readConfig( posfname, std::ifstream::in);
 
-    simProg << "The starting x-coordinate of the crystal: "<< xind_min << std::endl;
-    simProg << "The starting y-coordinate of the crystal: "<< yind_min << std::endl;
-    simProg << "The starting z-coordinate of the crystal: "<< zind_min << std::endl;
+if ( ! readConfig ) { simProg << "*** The file " << posfname << " could not be opened/ does not exist *** \n Aborting !! " << std::endl; abort(); }
 
-    simProg << "The ending x-coordinate of the crystal: "<< xind_max << std::endl;
-    simProg << "The ending y-coordinate of the crystal: "<< yind_max << std::endl;
-    simProg << "The ending z-coordinate of the crystal: "<< zind_max << std::endl;
+char dummyString[80];
+char dummyChar;
+char type;
+int flag =0;
+int pCount =0;
+double offSet = 0.000001;
 
-    pCount = 0;
-    for ( i = fluidType1_idxStart; i <= fluidType1_idxEnd; ++i ){
+readConfig >> npart;
 
-        // generate random velocities
-        rand_gen_velx = ((double) rand() / (RAND_MAX));
-        rand_gen_vely = ((double) rand() / (RAND_MAX));
-        rand_gen_velz = ((double) rand() / (RAND_MAX));
+for ( j = 1 ; j <= npart ; ++ j ){	
 
-        // generate random positions
-        xind = ((double) rand() / (RAND_MAX)) * ( boxEdge[x] );
-        yind = ((double) rand() / (RAND_MAX)) * ( boxEdge[y] );
-        zind = ((double) rand() / (RAND_MAX)) * ( 0.5 * boxEdge[z] );
+    // generate random velocities
+    rand_gen_velx = 0.;
+    rand_gen_vely = 0.;
+    rand_gen_velz = 0.;
 
-        // initializing particle radius, mass, position and velocity
-        particles.push_back({1.0,1.0,{xind, yind, zind},{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 1});
-        
-        pCount++;
-    }
+    readConfig >> type;
+    readConfig >> xind;
+    readConfig >> yind;
+    readConfig >> zind;
 
-    simProg << "finished initialization of " << pCount << " type 1 particles inside crystal lattice" << std::endl;
-    simProg << "***************************************************" << std::endl;
+    if ( type == 'H' )
+        particles.push_back( {1.0 ,1.0 , {xind + offSet, yind + offSet, zind + offSet} ,{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 1} );
+    else if ( type == 'C' )
+        particles.push_back( {1.0 ,1.0 , {xind + offSet, yind + offSet, zind + offSet} ,{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 2} );
 
-    pCount = 0;
-    for ( i = fluidType2_idxStart; i <= fluidType2_idxEnd; ++i ){
+    pCount++;
+    // simProg << type << "\t" << xind << "\t" << yind << "\t" << zind << "\t" << rand_gen_velx << "\t" << rand_gen_vely << "\t" << rand_gen_velz << std::endl; 
+}
 
-        // generate random velocities
-        rand_gen_velx = ((double) rand() / (RAND_MAX));
-        rand_gen_vely = ((double) rand() / (RAND_MAX));
-        rand_gen_velz = ((double) rand() / (RAND_MAX));
+simProg << "\n  " << npart << " particles making the fcc lattice were read" << std::endl;
 
-        // generate random positions
-        xind = ((double) rand() / (RAND_MAX)) * ( boxEdge[x] );
-        yind = ((double) rand() / (RAND_MAX)) * ( boxEdge[y] );
-        zind = 0.5 * boxEdge[z] + ((double) rand() / (RAND_MAX)) * ( 0.5 * boxEdge[z] );
-
-        // initializing particle radius, mass, position and velocity
-        particles.push_back({1.0,1.0,{xind, yind, zind},{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 2});
-        
-        pCount++;
-    }
-
-    simProg << "finished initialization of " << pCount << " type 2 particles inside crystal lattice" << std::endl;
-    simProg << "***************************************************" << std::endl;
-
-#else
-    simProg << "***************************************************" << std::endl;
-    simProg << "Started initialization of the single-component FCC crystal" << std::endl;
-
-    simProg << "The starting x-coordinate of the crystal: "<< xind_min << std::endl;
-    simProg << "The starting y-coordinate of the crystal: "<< yind_min << std::endl;
-    simProg << "The starting z-coordinate of the crystal: "<< zind_min << std::endl;
-
-    simProg << "The ending x-coordinate of the crystal: "<< xind_max << std::endl;
-    simProg << "The ending y-coordinate of the crystal: "<< yind_max << std::endl;
-    simProg << "The ending z-coordinate of the crystal: "<< zind_max << std::endl;
-
-    for ( i=1; i <= npart; ++i ){
-        // generate random positions
-        xind = ((double) rand() / (RAND_MAX)) * ( boxEdge[x] );
-        yind = ((double) rand() / (RAND_MAX)) * ( boxEdge[y] );
-        zind = ((double) rand() / (RAND_MAX)) * ( boxEdge[z] );
-
-        // generate random velocities
-        rand_gen_velx = ((double) rand() / (RAND_MAX));
-        rand_gen_vely = ((double) rand() / (RAND_MAX));
-        rand_gen_velz = ((double) rand() / (RAND_MAX));
-
-        // initializing particle radius, mass, position and velocity
-        particles.push_back({1.0,1.0,{xind, yind, zind},{rand_gen_velx, rand_gen_vely, rand_gen_velz}, 1});
-    } 
-
-simProg << "finished initialization of  " << npart << " particles inside crystal lattice" << std::endl;
-simProg << "***************************************************" << std::endl;
-#endif
-
+readConfig.close();
